80201fe5f5c145bfb13e30955eeb16483d3d873e
justin <shieh_justin@yahoo.com>
2019-11-19 21:29:51 -0800

    create function to modify ingredient amount

--- frontend/src/util/fridge_api_util.js
+++ frontend/src/util/fridge_api_util.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-// export const fetchFridge = userId => {
-//   return axios.get(`/api/fridge/${userId}`);
-// };
-
 export const fetchFridge = (userId) => {
   return axios.get(`/api/fridge/${userId}`);
 };
 
 export const addIngredientToFridge = (userId, ingredient) => {
-  return axios.patch(`/api/fridge/${userId}/add`, ingredient);
+  return axios.patch(`/api/fridge/${userId}/addNewIngredient`, ingredient);
+};
+
+export const modifyIngredient = (userId, ingredientId, amount) => {
+  return axios.patch(`/api/fridge/${userId}/modifyIngredient`, {ingredientId, amount});
 };
--- routes/api/fridge.js
+++ routes/api/fridge.js
@@ -6,35 +6,32 @@ const Fridge = require('../../models/fridge');
 router.get("/test", (req, res) => res.json({ msg: "This is the fridge route" }));
 
 router.get('/:userId', (req, res) => {
-  console.log(req.params.userId);
   Fridge.findOne({ userId: req.params.userId })
-    .then(fridge => res.json(fridge))
-    .catch(err => res.status(400).json(err));
+  .then(fridge => res.json(fridge))
+  .catch(err => res.status(400).json(err));
 });
 
-router.patch('/:userId/add', (req, res) => {
-  console.log(req.params);
-  console.log(req.body);
+router.patch('/:userId/addNewIngredient', (req, res) => {
   let update = { "$set": {}};
   let options = { "upsert": true };
   update["$set"]["ingredients." + req.body.ingredientId] = req.body;
-
-   Fridge.findOneAndUpdate({ userId: req.params.userId }, 
+  
+  Fridge.findOneAndUpdate({ userId: req.params.userId }, 
     update, options, function (err, data){
       if(err) return res.send(500, {error: err});
+      console.log(data);
       return res.send("successfully saved");
-    });
-      // .then(fridge => res.json(fridge))
-      // .catch(err => res.status(400).json(err));
-
+  });
+    // .then(fridge => res.json(fridge))
+    // .catch(err => res.status(400).json(err));   
 });
 
+router.patch('/:userId/modifyIngredient', (req, res) => {
+  console.log(req.body);
+  Fridge.findOne({ userId: req.params.userId })
+  .then(fridge => res.json(fridge))
+  .catch(err => res.status(400).json(err));
+});
 
-
-// router.post('/new', (req, res) => {
-//   const newFridge = new Fridge({
-
-//   });
-// });
-
+  
 module.exports = router;
