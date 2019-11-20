ed6b8209d639365594ed257fca878a781eaa766b
youngjunna <yjna4006@gmail.com>
2019-11-19 21:59:20 -0800

    Finish nutrition req calculator

--- frontend/src/util/calorie_util.js
+++ frontend/src/util/calorie_util.js
@@ -1,3 +1,4 @@
+// Returns daily calorie requirement based on personal profile
 export const calorieCalc = (weight, height, age, gender, activityLevel) => {
   let baseMetabolism;
   if (gender === "F") {
@@ -20,5 +21,16 @@ export const calorieCalc = (weight, height, age, gender, activityLevel) => {
   // Diet goals; 3500 calories a week deficit for 1 pound
 };
 
-
+// calculates % of week's nutritions you should be getting from # of meals
+// selected, and how much of that is being provided by the selected meals;
+export const nutritionCalc = (dailyCal, mealCount, totalCarb, totalFat, totalProtein) => {
+  let protein = 50 * dailyCal / 2000 * 7 * (mealCount/19); // weighed a bit more for these meals being 'bigger'
+  let carb = 300 * dailyCal / 2000 * 7 * (mealCount / 19);
+  let protein = 65 * dailyCal / 2000 * 7 * (mealCount / 19);
+  
+  // % of total protein 
+  totalProtein / protein; 
+  totalCarb / carb;
+  totalFat / fat;
+};
 
