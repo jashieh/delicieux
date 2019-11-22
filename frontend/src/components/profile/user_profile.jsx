e62c46999ef5f8452016a48f248d9794716696b8
justin <shieh_justin@yahoo.com>
2019-11-21 21:56:50 -0800

    add character

--- frontend/src/components/profile/user_profile.jsx
+++ frontend/src/components/profile/user_profile.jsx
@@ -17,15 +17,26 @@ class UserProfile extends React.Component {
         <div>
           Name: { this.props.user.name[0].toUpperCase() + this.props.user.name.slice(1)}
         </div>
-        <div>
+        <div className="profile-item-container">
           <div>
-            Gender
+            Genders
           </div>
-          
+          <div>
+            { this.props.user.gender }
+          </div>
         </div>
-        <div>
-          Height: { this.props.user.height }
+        <div className="profile-item-container">
+          Height
+          <div>
+            { this.props.user.height } cm
+          </div>
         </div>     
+        <div className="profile-item-container">
+          Current Weight
+          <div>
+            { this.props.user.weight } kg
+          </div>
+        </div> 
       </div>
     );
   }
