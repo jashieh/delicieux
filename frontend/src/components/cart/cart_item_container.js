d16e18b515dc8f073971943045968ac437a4d6c9
Yuci Shen <36720898+yshen901@users.noreply.github.com>
2019-11-22 10:20:18 -0800

    Remove asynchronicity issue from cart add as well

--- frontend/src/components/cart/cart_item_container.js
+++ frontend/src/components/cart/cart_item_container.js
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
 
 import CartItem from './cart_item';
-import { removeCartMeal, addCartMeal } from '../../actions/cart_actions';
+import { removeCartMeal, addCartMeal, getCart } from '../../actions/cart_actions';
 import { getRecipeDB } from '../../actions/recipe_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   time: ownProps.time,
   date: state.session.currentDate,
-  
+  userId: state.session.user.id,
+
   cart: state.entities.cart,
   recipes: state.entities.recipes,
 });
@@ -16,6 +17,7 @@ const mapDispatchToProps = dispatch => ({
   removeCartMeal: (cartId, mealInfo) => dispatch(removeCartMeal(cartId, mealInfo)),
   getRecipeDB: (recipeId) => dispatch(getRecipeDB(recipeId)),
   addCartMeal: (cartId, mealInfo) => dispatch(addCartMeal(cartId, mealInfo)),
+  getCart: (userId) => dispatch(getCart(userId)),
 });
 
 export default connect(
