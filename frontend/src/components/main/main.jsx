1ec7b19a7344264373a38c839053a608ee108ed5
justin <shieh_justin@yahoo.com>
2019-11-24 20:26:58 -0800

    add details page to splash

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -9,6 +9,8 @@ import ChooseMeal from '../stylesheets/assets/choose_meal.gif';
 import PieChart from '../stylesheets/assets/pie_chart.gif';
 import LineChart from '../stylesheets/assets/line_chart.gif';
 import GroceryGif from '../stylesheets/assets/grocery.gif';
+import CookingPic from '../stylesheets/assets/cooking.jpg';
+import MealPlan from '../stylesheets/assets/meal_plan.jpg';
 
 
 class MainPage extends React.Component {
@@ -27,9 +29,9 @@ class MainPage extends React.Component {
                         <input type="radio" name="radio-set" defaultChecked={false} id="st-control-1" />
                         <a href="#st-panel-1" className="splash-footer-tab">délicieux?</a>
                         <input type="radio" name="radio-set" id="st-control-2" />
-                        <a href="#st-panel-2" className="splash-footer-tab">How it works</a>
+                        <a href="#st-panel-2" className="splash-footer-tab">The experience</a>
                         <input type="radio" name="radio-set" id="st-control-3" />
-                        <a href="#st-panel-3" className="splash-footer-tab">Tranquillity</a>
+                        <a href="#st-panel-3" className="splash-footer-tab">How it works</a>
                         <input type="radio" name="radio-set" id="st-control-4" />
                         <a href="#st-panel-4" className="splash-footer-tab">Positivity</a>
                         <input type="radio" name="radio-set" id="st-control-5" />
@@ -37,7 +39,7 @@ class MainPage extends React.Component {
 
                         <div className="st-scroll">
 
-                            <section className="st-panel" id="st-panel-1">
+                            <section className="st-panel st-color" id="st-panel-1">
                                 <div className="img-container">
                                     <img src={Background1} alt=""
                                     className="splash-img"/>
@@ -71,7 +73,34 @@ class MainPage extends React.Component {
                                     className="splash-img"/>
                                 </div>
                                 <div className="st-deco">
-                                    {/* <i class="fas fa-paw apron"></i> */}
+                                </div>
+                                <div className="splash-middle-container">
+                                    <h2 className="the-experience">Meal planning made easy</h2>
+                                    <div className="the-experience-middle-container">
+                                        <div className="the-experience-left">
+                                            <div className="the-experience-sub-head">
+                                                délicieux is your smart cooking sidekick
+                                            </div>
+                                            <div className="the-experience-body">
+                                                Find the perfect recipe to make from the ingredients available in your fridge.
+                                            </div>
+                                            <div className="the-experience-body">
+                                                Reach your diet goals faster with detailed nutritional information and progress visualization.
+                                            </div>
+                                        </div>
+                                        <div className="the-experience-right">
+                                            <img src={MealPlan} alt="" className="the-experience-img"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section className="st-panel st-color" id="st-panel-3">
+                                <div className="img-container">
+                                    <img src={Background2} alt=""
+                                    className="splash-img"/>
+                                </div>
+                                <div className="st-deco">
                                 </div>
                                 <div className="splash-middle-container">
                                     <h2 className="how-it-works">How it works</h2>
@@ -114,16 +143,7 @@ class MainPage extends React.Component {
                                             </div>
                                         </div>
                                     </div>
-                                    {/* <p>Art party readymade beard labore cosby sweater culpa. Art party whatever incididunt, scenester umami polaroid tofu.</p> */}
                                 </div>
-                            </section>
-
-                            <section className="st-panel" id="st-panel-3">
-                                <div className="st-deco">
-
-                                </div>
-                                <h2>Tranquillity</h2>
-                                <p>Sint aute occaecat id vice. Post-ironic fap pork belly next level godard, id fanny pack williamsburg forage truffaut.</p>
                             </section>
 
                             <section className="st-panel st-color" id="st-panel-4">
