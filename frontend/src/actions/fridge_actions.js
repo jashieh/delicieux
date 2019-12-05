80142e5a005c2b09ffab0b2e1c1b4b3abae0d577
Yuci Shen <36720898+yshen901@users.noreply.github.com>
2019-12-05 05:49:51 -0800

    Change fetchFridge to fetch ingredient info if there are entries with missing info

--- frontend/src/actions/fridge_actions.js
+++ frontend/src/actions/fridge_actions.js
@@ -1,4 +1,5 @@
-import * as APIUtil from '../util/fridge_api_util';
+import * as FridgeAPIUtil from '../util/fridge_api_util';
+import * as IngredientAPIUtil from '../util/ingredient_api_util';
 
 export const RECEIVE_FRIDGE = 'RECIEVE_FRIDGE';
 export const RECEIVE_FRIDGE_INGREDIENT = 'RECEIVE_FRIDGE_INGREDIENT';
@@ -19,18 +20,54 @@ const removeFridgeIngredient = id => ({
   id
 });
 
-export const fetchFridge = (userId) => dispatch => (APIUtil.fetchFridge(userId)
-  .then(({ data }) => dispatch(receiveFridge(data)))
+// Add a condition where if there is an ingredient with just an amount but no
+// information, then fetch the ingredient information. Such entries can happen 
+// when unmaking items. 
+export const fetchFridge = (userId) => dispatch => (
+  FridgeAPIUtil
+    .fetchFridge(userId)
+    .then(({ data }) => {
+      let results = 0;
+      let ids = Object.keys(data.ingredients);
+      for (let i = 0; i < ids.length; i++) {
+        let id = ids[i];
+        if (data.ingredients[id].amount && !data.ingredients[id].id) {
+          results++;
+          IngredientAPIUtil
+            .getIngredientById(id)
+            .then(ingredient => {
+              debugger;
+              FridgeAPIUtil
+              .addFridgeIngredient(userId, ingredient.data, data.ingredients[id].amount)
+              .then(() => {
+                results--;
+                if (results === 0) {
+                  debugger;
+                  dispatch(receiveFridge(data)) 
+                }
+              })
+            })
+            .catch(() => {
+              results--; //if ingredient can't be found but is in database anyways, must remove that entry
+              if (results === 0) {
+                dispatch(receiveFridge(data));
+              }
+            })
+        }
+      }
+    })
 );
 
-export const addFridgeIngredient = (userId, ingredient, amount) => dispatch => 
-  (APIUtil.addFridgeIngredient(userId, ingredient, amount).then(
-    ingredient => dispatch(receiveFridgeIngredient(ingredient)))
+export const addFridgeIngredient = (userId, ingredient, amount) => dispatch => (
+  FridgeAPIUtil
+    .addFridgeIngredient(userId, ingredient, amount)
+    .then(ingredient => dispatch(receiveFridgeIngredient(ingredient)))
 );
 
 export const modifyIngredient = (userId, ingredient, amount) => dispatch =>(
-  APIUtil.modifyIngredient(userId, ingredient, amount).then(
-    res => {
+  FridgeAPIUtil
+    .modifyIngredient(userId, ingredient, amount)
+    .then(res => {
       if(res.data === "") {
         dispatch(removeFridgeIngredient(ingredient.id));
       } else {
