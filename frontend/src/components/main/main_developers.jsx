b5053ace50f3e23a73a5f2d256af101023dbf353
Kelly <kjk224@cornell.edu>
2019-12-09 11:13:02 -0800

    dev-page-styling

--- frontend/src/components/main/main_developers.jsx
+++ frontend/src/components/main/main_developers.jsx
@@ -27,10 +27,30 @@ class MainDevelopers extends React.Component {
                 <div className="main-developer-name">Justin Shieh</div>
               </div>
               <div className="dev-right">
-                <a href="https://www.linkedin.com/in/shieh-justin/" className="main-developer-link">LinkedIn</a>
-                <a href="https://github.com/jashieh/" className="main-developer-link">GitHub</a>
-                <a href="https://github.com/yshen901" className="main-developer-link">Profile</a>
-                <a href="https://github.com/yshen901" className="main-developer-link">AngelList</a>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={LinkedIn} alt="" />
+                  </div>
+                  <a href="https://www.linkedin.com/in/shieh-justin/" className="main-developer-link">LinkedIn</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={GitHub} alt="" />
+                  </div>
+                  <a href="https://github.com/jashieh/" className="main-developer-link">GitHub</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={Profile} alt="" />
+                  </div>
+                  <a href="https://github.com/yshen901" className="main-developer-link">Profile</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={Angel} alt="" />
+                  </div>
+                  <a href="https://github.com/yshen901" className="main-developer-link">AngelList</a>
+                </div>
               </div>
             </div>
             <div className="main-developer">
@@ -41,10 +61,30 @@ class MainDevelopers extends React.Component {
                 <div className="main-developer-name">Kelly Ku</div>
               </div>
               <div className="dev-right">
-                <a href="https://www.linkedin.com/in/kelly-ku-642b49130/" className="main-developer-link">LinkedIn</a>
-                <a href="https://github.com/kellyk525" className="main-developer-link">GitHub</a>
-                <a href="https://github.com/yshen901" className="main-developer-link">Profile</a>
-                <a href="https://github.com/yshen901" className="main-developer-link">AngelList</a>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={LinkedIn} alt="" />
+                  </div>
+                  <a href="https://www.linkedin.com/in/kelly-ku-642b49130/" className="main-developer-link">LinkedIn</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={GitHub} alt="" />
+                  </div>
+                  <a href="https://github.com/kellyk525" className="main-developer-link">GitHub</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={Profile} alt="" />
+                  </div>
+                  <a href="https://github.com/yshen901" className="main-developer-link">Profile</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={Angel} alt="" />
+                  </div>
+                  <a href="https://github.com/yshen901" className="main-developer-link">AngelList</a>
+                </div>
               </div>
             </div>
           </div>
@@ -91,10 +131,30 @@ class MainDevelopers extends React.Component {
                 <div className="main-developer-name">Youngjun Na</div>
               </div>
               <div className="dev-right">
-                <a href="https://www.linkedin.com/in/youngjun-na-93a3606a/" className="main-developer-link">LinkedIn</a>
-                <a href="https://github.com/youngjun-na" className="main-developer-link">GitHub</a>
-                <a href="https://github.com/yshen901" className="main-developer-link">Profile</a>
-                <a href="https://github.com/yshen901" className="main-developer-link">AngelList</a>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={LinkedIn} alt="" />
+                  </div>
+                  <a href="https://www.linkedin.com/in/youngjun-na-93a3606a/" className="main-developer-link">LinkedIn</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={GitHub} alt="" />
+                  </div>
+                  <a href="https://github.com/youngjun-na" className="main-developer-link">GitHub</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={Profile} alt="" />
+                  </div>
+                  <a href="https://github.com/yshen901" className="main-developer-link">Profile</a>
+                </div>
+                <div className="dev-link">
+                  <div className="dev-logo">
+                    <img src={Angel} alt="" />
+                  </div>
+                  <a href="https://github.com/yshen901" className="main-developer-link">AngelList</a>
+                </div>
               </div>
             </div>
           </div>
