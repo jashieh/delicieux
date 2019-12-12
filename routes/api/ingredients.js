80e17682a3baee21c7969cb95b51d02b4bae8e66
justin <shieh_justin@yahoo.com>
2019-12-12 09:37:36 -0800

    round ingredient amounts up so 0 isnt displayed at low amounts

--- routes/api/ingredients.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Ingredient = require('../../models/ingredient');
-
-router.get('/:ingredientId', (req, res) => {
-  
-});
